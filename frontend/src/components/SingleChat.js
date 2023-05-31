d0a12c1ab1d7f9f69f770f0a60fc10131857d3fe
Ayush Dwivedi <72956835+adayush17@users.noreply.github.com>
2023-05-31 21:16:43 +0530

    Update SingleChat.js

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -20,7 +20,7 @@ import io from "socket.io-client";
 import Lottie from "react-lottie";
 import animationData from "../animations/typing.json";
 
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://talk-a-tive-w0n3.onrender.com/" // http://localhost:5000";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
